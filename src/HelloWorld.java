17de8f9e9783ed13b8b608229bd5e30e72d64489
LAPTOP-QI8D7EP6\dannm <dmcdonough@wpi.edu>
2017-03-16 16:41:02 -0400

    Added names

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,18 @@
+import com.sun.prism.shader.Solid_TextureYV12_AlphaTest_Loader;
+
 public class HelloWorld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("William Schwartz");
         System.out.println("Dylan McKillip");
+        System.out.println("Daniel McDonough");
+        System.out.println("Samantha Comeau");
+        System.out.println("Brian Copeland");
+        System.out.println("Alazar Genene");
+        System.out.println("Eric Peterson");
+        System.out.println("Ryan Benasutti");
+        System.out.println("Saul Woolf");
     }
 
 }
